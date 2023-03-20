e20784738f2cdc867658817941b33d89c402d32d
Felixjuteau <felixjuteau@gmail.com>
2023-03-20 10:44:27 +0100

    first commit

---
+++ SolutionBDDPersonels/AppliWPF-BDD-Personels/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BddpersonnelContext;
+using biblioBDDPersonels1;
+
+
+namespace AppliWPF_BDD_Personels
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CBDDPersonels1 bddPersonels=null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            bddPersonels= new CBDDPersonels1();
+            List<Service> services=bddPersonels.GetAllServices();
+            datagridServies.ItemsSource = services;
+        }
+    }
+}
---
+++ SolutionBDDPersonels/biblioBDDPersonels1/CBDDPersonels1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BddpersonnelContext;
+namespace biblioBDDPersonels1
+{
+    public class CBDDPersonels1
+    {
+        private BddpersonnelDataContext dc=null;
+
+        public CBDDPersonels1()
+        {
+            dc = new BddpersonnelDataContext();
+        }
+        public List<Service> GetAllServices()
+        {
+            try
+            {
+                return dc.Services.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
