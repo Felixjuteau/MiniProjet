4914f5f2cd413326a36a08088b682ad700f47a58
Felixjuteau <felixjuteau@gmail.com>
2023-04-05 16:13:30 +0200

    finito le projet

---
+++ MiniProjetApp-master/SolutionBDDPersonels/AppliWPF-BDD-Personels/Window1.xaml.cs
@@ -0,0 +1,356 @@
+﻿using BddpersonnelContext;
+using biblioBDDPersonels1;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace AppliWPF_BDD_Personels
+{
+    /// <summary>
+    /// Logique d'interaction pour Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private CBDDPersonels1 bddPersonels = null;
+        public Window1()
+        {
+            InitializeComponent();
+            bddPersonels = new CBDDPersonels1();
+            Trombinoscope();
+            ComboBox();
+            BackgroundTask();
+        }
+
+
+        private void Button_envoyer(object sender, RoutedEventArgs e)
+        {
+            Personnel personnel = new Personnel();
+            personnel.Nom = TBNom.Text.ToString();
+            personnel.Prenom = TBPrenom.Text.ToString();
+            //recuperer image et la mettre dans le personnel
+            List<Service> services = bddPersonels.GetAllServices();
+            List<Fonction> fonctions = bddPersonels.GetAllFonctions();
+            foreach (Service service in services)
+            {
+                if (CBService.Text == service.Intitule)
+                {
+                    personnel.Service = service;
+                    personnel.IdService = service.Id;
+                }
+            }
+            foreach (Fonction fonction in fonctions)
+            {
+                if (CBFonction.Text == fonction.Intitule)
+                {
+                    personnel.Fonction = fonction;
+                    personnel.IdFonction = fonction.Id;
+                }
+            }
+        }
+        private void Trombinoscope()
+        {
+            try
+            {
+                List<Personnel> personnels = bddPersonels.GetAllPersonnels();
+                foreach (Personnel personnel in personnels)
+                {
+
+                    ListBoxTrom.Items.Add(stack(personnel));
+
+                }
+            }
+            catch (Exception ex) { throw ex; }
+
+        }
+        public void LoadImage(byte[] imageData, Image icon)
+        {
+            BitmapImage bitmapImage = new BitmapImage();
+
+            try
+            {
+                using (MemoryStream memoryStream = new MemoryStream(imageData))
+                {
+                    bitmapImage.BeginInit();
+                    bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmapImage.StreamSource = memoryStream;
+                    bitmapImage.EndInit();
+                }
+
+                icon.Source = bitmapImage;
+            }
+            catch { }
+        }
+        public byte[] ReverseImage(Image image)
+        {
+            byte[] imageData = null;
+
+            try
+            {
+                BitmapSource bitmapSource = (BitmapSource)image.Source;
+                JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(bitmapSource));
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    encoder.Save(ms);
+                    imageData = ms.ToArray();
+                }
+            }
+            catch { }
+
+            return imageData;
+        }
+        public StackPanel stack(Personnel personnel)
+        {
+            try
+            {
+                StackPanel stackPanel = new StackPanel();
+                Image image = new Image();
+                TextBlock textBlock = new TextBlock();
+                image.Name = "I" + personnel.Id.ToString();
+                textBlock.Name = "TB" + personnel.Id.ToString();
+                LoadImage(personnel.Photo, image);
+                textBlock.Text = "Nom: " + personnel.Nom + "\nPrénom: " + personnel.Prenom;
+                textBlock.TextWrapping = TextWrapping.Wrap;
+                textBlock.TextAlignment = TextAlignment.Center;
+                image.Width = image.Height = 150;
+                textBlock.Width = 150;
+                stackPanel.Width = 150;
+                stackPanel.Children.Add(image);
+                stackPanel.Children.Add(textBlock);
+                stackPanel.Name = "SP" + personnel.Id;
+                return stackPanel;
+            }
+            catch (Exception ex) { throw ex; }
+        }
+
+
+
+        private void BtPhoto_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openDialog = new OpenFileDialog();
+                openDialog.Filter = "Image files |* .bmp;*.jpg;*.png";
+                openDialog.FilterIndex = 1;
+                if (openDialog.ShowDialog() == true)
+                {
+                    imagePicture.Source = new BitmapImage(new Uri(openDialog.FileName));
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void ListBoxTrom_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                CheckInputs();
+                ListBox listBox = sender as ListBox;
+                StackPanel selectedStackPanel = listBox.SelectedItem as StackPanel;
+                if (selectedStackPanel != null)
+                {
+                    //je recupere l'image du stackpanel et je l'implante dans mon image de modif
+                    Image selectedImage = selectedStackPanel.Children.OfType<Image>().FirstOrDefault();
+                    imagePicture.Source = selectedImage.Source;
+
+                    //je recupere le texte du textBlock
+                    TextBlock selectedTextBlock = selectedStackPanel.Children.OfType<TextBlock>().FirstOrDefault();
+
+                    //je recupere le nom
+                    int found = selectedTextBlock.Text.IndexOf("\n");
+                    string PP = selectedTextBlock.Text.Substring(found + 8).Trim();
+
+                    String searchString = ":";
+                    int startIndex = selectedTextBlock.Text.IndexOf(searchString) + 1;
+                    searchString = "Prénom";
+                    int endIndex = selectedTextBlock.Text.IndexOf(searchString) - 7;
+                    String NP = selectedTextBlock.Text.Substring(startIndex, endIndex + searchString.Length - startIndex).Trim();
+                    //MessageBox.Show(substring);
+                    //MessageBox.Show(PP);
+                    List<Personnel> personnels = bddPersonels.GetAllPersonnels();
+
+                    BtModif.IsEnabled = true;
+                    BtSupprimer.IsEnabled = true;
+                    if (personnels != null)
+                    {
+                        foreach (Personnel personnel in personnels)
+                        {
+                            if (NP == personnel.Nom && PP == personnel.Prenom)
+                            {
+
+                                TBNom.Text = personnel.Nom.ToString();
+                                TBPrenom.Text = personnel.Prenom;
+                                TBTelephone.Text = personnel.Telephone;
+                                CBService.Text = personnel.Service.Intitule;
+                                CBFonction.Text = personnel.Fonction.Intitule;
+                                TBId.Text = personnel.Id.ToString();
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception("listPersonnel vide");
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void ComboBox()
+        {
+            try
+            {
+                List<Service> services = bddPersonels.GetAllServices();
+                List<Fonction> fonctions = bddPersonels.GetAllFonctions();
+                foreach (Service service in services)
+                {
+                    CBService.Items.Add(service.Intitule);
+
+                }
+                foreach (Fonction fonction in fonctions)
+                {
+                    CBFonction.Items.Add(fonction.Intitule);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void BtAjouter_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Personnel personnel = new Personnel();
+                personnel.Nom = TBNom.Text.ToString();
+                personnel.Prenom = TBPrenom.Text.ToString();
+                personnel.Photo = ReverseImage(imagePicture);
+                List<Service> services = bddPersonels.GetAllServices();
+                List<Fonction> fonctions = bddPersonels.GetAllFonctions();
+                foreach (Service service in services)
+                {
+                    if (CBService.Text == service.Intitule)
+                    {
+                        personnel.IdService = service.Id;
+                    }
+                }
+                foreach (Fonction fonction in fonctions)
+                {
+                    if (CBFonction.Text == fonction.Intitule)
+                    {
+                        personnel.IdFonction = fonction.Id;
+                    }
+                }
+                bddPersonels.AjouterPersonnel(personnel);
+                ListBoxTrom.Items.Clear();
+                Trombinoscope();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        private void BtModif_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (TBNom.Text != "" || TBPrenom.Text != "")
+                {
+                    Personnel personnel = new Personnel();
+
+                    personnel.Nom = TBNom.Text;
+                    personnel.Prenom = TBPrenom.Text;
+                    personnel.Telephone = TBTelephone.Text;
+                    personnel.IdService = bddPersonels.GetServicebyintitule(CBService.SelectedItem.ToString()).Id;
+                    personnel.IdFonction = bddPersonels.GetFonctionbyintitule(CBFonction.SelectedItem.ToString()).Id;
+                    personnel.Photo = ReverseImage(imagePicture);
+                    personnel.Id = Convert.ToInt32(TBId.Text);
+                    bddPersonels.ModifPersonel(personnel);
+                    ListBoxTrom.Items.Clear();
+                    Trombinoscope();
+                }
+                else
+                {
+                    throw new Exception("erreur Modification");
+                }
+            }
+            catch (Exception ex) { throw ex; }
+        }
+
+        private void BtSupprimer_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (TBNom.Text != "" || TBPrenom.Text != "")
+                {
+
+                    int Id = Convert.ToInt32(TBId.Text);
+                    bddPersonels.SuppPersonnel(Id);
+                    ListBoxTrom.Items.Clear();
+                    Trombinoscope();
+                }
+            }
+            catch (Exception ex) { throw ex; }
+        }
+
+
+
+        private void BackgroundTask()
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1); // intervalle de 1 seconde
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            CheckInputs();
+        }
+        private void TBNom_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckInputs();
+        }
+
+        private void TBPrenom_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckInputs();
+        }
+
+        private void CheckInputs()
+        {
+            if (ListBoxTrom.SelectedItem == null && !string.IsNullOrEmpty(TBNom.Text) && !string.IsNullOrEmpty(TBPrenom.Text))
+            {
+                BtAjouter.IsEnabled = true;
+            }
+            else
+            {
+                BtAjouter.IsEnabled = false;
+            }
+        }
+    }
+}
---
+++ MiniProjetApp-master/SolutionBDDPersonels/biblioBDDPersonels1/CBDDPersonels1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BddpersonnelContext;
+using Devart.Common;
+using System.IO;
+using System.Drawing;
+using System.Runtime.InteropServices.ComTypes;
+using System.Windows;
+using static System.Net.Mime.MediaTypeNames;
+using System.Security.Policy;
+
+namespace biblioBDDPersonels1
+
+{
+    public class CBDDPersonels1
+    {
+        private BddpersonnelDataContext dc=null;
+
+        //constructeur de base pour accéder à la bdd
+        public CBDDPersonels1()
+        {
+            dc = new BddpersonnelDataContext();
+        }
+
+        //constructeur pour accéder à la base en gestionnaire(mais pas uniquement)
+        public CBDDPersonels1( string UserId, string Password, string Host, string Database)
+        {
+            
+            string connectionString = "User Id="+UserId+";Password="+Password+";Host="+Host+";Database="+Database+";Persist Security Info=True";
+            dc = new BddpersonnelDataContext(connectionString);
+           
+        }
+
+        public  User GetUser()
+        {
+            try
+            {
+                
+                return dc.Users.FirstOrDefault();
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+        //tableau de services à revoir
+        public List<Service> GetAllServices()
+        {
+            try
+            {
+                return dc.Services.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+        //tableau des fonctions
+
+        public List<Fonction> GetAllFonctions()
+        {
+            try
+            {
+                return dc.Fonctions.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+        //tableau du personnels
+        public List<Personnel> GetAllPersonnels()
+        {
+            try
+            {
+                return dc.Personnels.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    
+    public List<Service> GetServicestrier()
+    {
+        //List<Service> services = dc.Services.ToList();
+        //services.Sort();
+        //return services;
+        return dc.Services.OrderBy(y => y.Intitule).ToList();
+    }
+    public Service GetServicebyintitule(string intitule)
+    {
+        return dc.Services.Where(x => x.Intitule == intitule).FirstOrDefault();
+    }
+    public Fonction GetFonctionbyintitule(string intitule)
+    {
+        return dc.Fonctions.Where(x => x.Intitule == intitule).FirstOrDefault();
+    }
+
+
+    public Personnel GetPersonnel(string _Prenom, string _Nom, int _IdService, int _IdFonction, string _Telephone, byte[] _Photo)
+    {//permet de recuperer 
+        Personnel personnel = new Personnel();
+        personnel.Prenom = _Prenom;
+        personnel.Nom = _Nom;
+        personnel.IdService = _IdService;
+        personnel.IdFonction = _IdFonction;
+        personnel.Telephone = _Telephone;
+        personnel.Photo = _Photo;
+        return personnel;
+    }
+
+    public void ModifPersonel(Personnel personnel)
+    {
+        dc.Connection.Open();
+        using (System.Data.Common.DbTransaction transaction
+        = dc.Connection.BeginTransaction())
+        {
+
+            try
+            {
+                int id = personnel.Id;
+                Personnel personnel2 = dc.Personnels.Single(x => x.Id == id);
+                dc.SubmitChanges();
+                dc.Personnels.InsertOnSubmit(personnel);
+                dc.Personnels.DeleteOnSubmit(personnel2);
+                dc.SubmitChanges();
+                transaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                throw ex;
+            };
+        }
+    }
+
+    public void SuppPersonnel(int Id)
+    {
+        try
+        {
+
+            Personnel personnel2 = dc.Personnels.Single(x => x.Id == Id);
+            dc.Personnels.DeleteOnSubmit(personnel2);
+            dc.SubmitChanges();
+        }
+        catch (Exception ex) { throw ex; };
+    }
+    public void AjouterPersonnel(Personnel personnel)
+    {
+        try
+        {
+            dc.Personnels.InsertOnSubmit(personnel);
+            dc.SubmitChanges();
+        }
+        catch (Exception ex) { throw ex; };
+    }
+    //return bdd.Personnels.Where(xx => x.Service.Id == service Id.OrderBy(y=> y.nom).then(y=>u.Prenom).ToList();
+
+}
+}
+
+
+
